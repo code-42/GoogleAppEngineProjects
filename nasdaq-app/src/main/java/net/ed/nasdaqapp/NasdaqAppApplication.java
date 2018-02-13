0f1608de7add6b4616cf32fb6fa2be8c4a575958
Edward Dupre <edwd42@gmail.com>
2018-02-13 14:03:25 -0500

    initial commit

---
+++ nasdaq-app/src/main/java/net/ed/nasdaqapp/NasdaqAppApplication.java
@@ -0,0 +1,12 @@
+package net.ed.nasdaqapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasdaqAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NasdaqAppApplication.class, args);
+	}
+}
