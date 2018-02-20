db017aa5290304fb7094ea347ac3c5192c4bcef9
Edward Dupre <edwd42@gmail.com>
2018-02-19 23:15:17 -0500

    initial commit

---
+++ OptionsAnalyzer/src/main/java/net/ed/HelloAppEngine.java
@@ -0,0 +1,33 @@
+package net.ed;
+
+/**
+ * Copyright 2018 Edward Dupre
+ * this project starts here --> 
+ * https://cloud.google.com/eclipse/docs/creating-new-webapp
+ * 
+ */
+
+import java.io.IOException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(
+    name = "HelloAppEngine",
+    urlPatterns = {"/hello"}
+)
+public class HelloAppEngine extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) 
+      throws IOException {
+      
+    response.setContentType("text/plain");
+    response.setCharacterEncoding("UTF-8");
+
+    response.getWriter().print("Hello App Engine!\r\n");
+
+  }
+}
