47252244cf36675ec838ec404ba7dde6bb1257e1
Edward Dupre <edwd42@gmail.com>
2018-02-25 11:12:31 -0500

    add Selenium webdriver and opens YahooFinance homepagae

---
+++ YahooFinanceScraper/src/main/java/net/ed/YahooFinanceLogin.java
@@ -0,0 +1,63 @@
+package net.ed;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Servlet implementation class YahooFinanceLogin
+ */
+@WebServlet("/YahooFinanceLogin")
+public class YahooFinanceLogin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public YahooFinanceLogin() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String key = "webdriver.chrome.driver";
+		String value = "/Users/melocal/Applications/lib/chromedriver";
+		System.setProperty(key, value);
+		
+		WebDriver driver = new ChromeDriver(); // launch chrome
+		driver.manage().window().maximize(); // maximize chrome browser
+		driver.manage().deleteAllCookies(); // exactly what it says
+		
+		// dynamic wait
+		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+		String url = "https://finance.yahoo.com";
+		driver.get(url);
+
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ YahooFinanceScraper/src/main/webapp/index.jsp
@@ -0,0 +1,26 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+This is YahooFinanceScraper index.jsp<br>
+edit web.xml to run this file on startup<br>
+The time on the server is <%= new java.util.Date() %><br>
+Request user agent: <%= request.getHeader("User-Agent") %><br>
+Local language: <%= request.getLocale() %>
+
+    <table>
+      <tr><td colspan="2" style="font-weight:bold;">Available Servlets:</td></tr>
+      <tr><td><a href="YahooFinanceLogin">Yahoo Finance Login</a></td></tr>
+
+    </table>
+
+Enter a ticker symbol to download QuoteTable file: 
+
+
+</body>
+</html>
