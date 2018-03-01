ce26796c6a950dfd3ac9fab209da8bcb13bc7245
Edward Dupre <edwd42@gmail.com>
2018-03-01 13:27:57 -0500

    add test getCookies_Test()

--- YahooFinanceScraper/src/test/java/net/ed/LoginServletTest.java
+++ YahooFinanceScraper/src/test/java/net/ed/LoginServletTest.java
@@ -2,9 +2,13 @@ package net.ed;
 
 import static org.testng.Assert.assertEquals;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
@@ -33,7 +37,7 @@ public class LoginServletTest {
 	}
 	
 	@Test(priority = 1)
-	public void getURLTest() {
+	public void getURL_Test() {
 		
 		try {
 			Thread.sleep(500);
@@ -51,13 +55,13 @@ public class LoginServletTest {
 	
 	
 	@Test(priority = 2)
-	public void enterUserNameTest() {
+	public void enterUserName_Test() {
 		
 		// sleep for a few milliseconds then click it
 		try {
-			Thread.sleep(5000);
+			Thread.sleep(500);
 			driver.findElement(By.name("username")).sendKeys("edev42@yahoo.com"); // fill in the blanks
-			Thread.sleep(5000);
+			Thread.sleep(500);
 			driver.findElement(By.name("signin")).click();
 			System.out.println("clicked Next button");
 			String expected = "Yahoo - login";
@@ -70,24 +74,62 @@ public class LoginServletTest {
 	}
 	
 	@Test(priority = 3)
-	public void enterUserPasswordTest() {
+	public void enterPassword_Test() {
 		
 		// sleep for a few milliseconds then click it
 		try {
-			Thread.sleep(5000);
+			Thread.sleep(500);
 			// the password is incorrect
 			driver.findElement(By.name("password")).sendKeys("incorrect42");
 			Thread.sleep(500);
 			driver.findElement(By.name("verifyPassword")).click();
+			Thread.sleep(500);
 			System.out.println("clicked Verify Password button");
-			Thread.sleep(5000);
-			String expected = "Yahoo - login";
+			String expected = "Yahoo";
 			String actual = driver.getTitle();
 			assertEquals(expected, actual);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	@Test(priority = 4)
+	public void getCookies_Test() {
+		// get cookies
+		// tutorial https://www.guru99.com/handling-cookies-selenium-webdriver.html
+        // create file named Cookies to store Login Information		
+        File file = new File("Cookies.data");
+	    try		
+	    {	  
+	        // Delete old file if exists
+			file.delete();		
+	        file.createNewFile();			
+	        FileWriter fileWrite = new FileWriter(file);							
+	        BufferedWriter Bwrite = new BufferedWriter(fileWrite);							
+	        	
+	        // loop for getting the cookie information 		
+	        for(Cookie ck : driver.manage().getCookies())							
+	        {			
+	            Bwrite.write((ck.getName()+";"+ck.getValue()+";"+ck.getDomain()+";"+ck.getPath()+";"+ck.getExpiry()+";"+ck.isSecure()));																									
+	            Bwrite.newLine();
+	            System.out.println("175. LoginServlet Selenium cookies...");
+	            System.out.println(ck.getName()+";"+ck.getValue()+";"+ck.getDomain()+";"+ck.getPath()+";"+ck.getExpiry()+";"+ck.isSecure());
+	        }			
+	        Bwrite.close();			
+	        fileWrite.close();	
+//	        response.sendRedirect("/YahooFinanceScraper");
+//	        exitApp();
+			String expected = "Yahoo";
+			String actual = driver.getTitle();
+			assertEquals(expected, actual);
+	    }
+	    catch(Exception ex)					
+	    {		
+	        ex.printStackTrace();			
+	    }
+	
+
 	}
 	
 	@AfterTest
