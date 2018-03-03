db2164c0b4a5f2a8e8325becd783f92b62c55ca1
Edward Dupre <edwd42@gmail.com>
2018-03-02 23:30:43 -0500

    test completed - now copy methods to YahooFinanceScraper.java

--- YahooFinanceScraper/src/main/java/net/ed/YahooFinanceScraper.java
+++ YahooFinanceScraper/src/main/java/net/ed/YahooFinanceScraper.java
@@ -3,13 +3,17 @@ package net.ed;
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.Set;
+
+//import static org.testng.Assert.assertEquals;
+
 import java.io.BufferedReader;		
 import java.io.BufferedWriter;		
 import java.io.File;		
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.Date;		
-import java.util.StringTokenizer;		
+import java.util.StringTokenizer;
+import java.util.concurrent.TimeUnit;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -21,6 +25,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Cookie;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
 import org.openqa.selenium.WebDriver;		
 
 
@@ -30,6 +35,7 @@ import org.openqa.selenium.WebDriver;
 @WebServlet("/YahooFinanceScraper")
 public class YahooFinanceScraper extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	static WebDriver driver;
 
     /**
      * @see HttpServlet#HttpServlet()
@@ -44,8 +50,43 @@ public class YahooFinanceScraper extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		launchBrowser();
+		
+		terminateBrowser();
+
+	}
 	
+	public void launchBrowser() {
+		// add support for chrome driver
+		String key = "webdriver.chrome.driver";
+		String valuex = "/Users/melocal/Applications/lib/chromedriver";
+		System.setProperty(key, valuex);
+		
+		driver = new ChromeDriver(); // launch chrome
+		driver.manage().window().maximize(); // maximize chrome browser
+//			driver.manage().deleteAllCookies(); // exactly what it says
+		
+		// dynamic wait
+		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+	}
+	
+	
+
+	
+	// now close the browser
+	public void terminateBrowser() {
+		// sleep for a few seconds then close chrome browser
+		try {
+			Thread.sleep(9000);
+			driver.quit();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	/**
